e5ddc5605705a273381dc13ce45de46f3ba4c867
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-08-17 18:12:16 +0530

    created classInput

---
+++ todo-list/src/components/classInput.jsx
@@ -0,0 +1,16 @@
+import React, {Component, components} from "react";
+
+class ClassInput extends Component{
+    constructor(props) {
+        super(props);
+
+        render() {
+            return (
+                <div>
+
+                </div>
+            )
+        }
+    }
+}
+export default ClassInput;
